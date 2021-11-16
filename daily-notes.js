2bec4622a3c88b19c08c91808bb4b8f087815c00
matt-rich-anderson <matt.rich.anderson@gmail.com>
2021-11-16 15:58:03 -0600

    changes

---
+++ daily-notes.js
@@ -0,0 +1,25 @@
+const notes = [
+    {
+        id: 1,
+        subject: "HTML",
+        date: "11/11/21",
+        feeling: "Good",
+        timeSpent: 5,
+    },
+    {
+        id: 2,
+        subject: "CSS",
+        date: "11/15/21",
+        feeling: "Apathetic",
+        timeSpent: 4, 
+    },
+    {
+        id: 3,
+        subject: "Javascript",
+        date: "11/16/21",
+        feeling: "Fearful",
+        timeSpent: 6, 
+    }
+]
+
+console.log(notes)
