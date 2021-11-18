08c2487f379cc252416df68f2c77dd1e12565c41
matt-rich-anderson <matt.rich.anderson@gmail.com>
2021-11-18 13:18:39 -0600

    through chp 10

--- daily-notes.js
+++ daily-notes.js
@@ -43,3 +43,36 @@ for (const note of notes) {
 for (const note of notes) {
     console.log(`On ${note.date} I spent ${note.timeSpent}hr/s studying ${note.subject} and I felt ${note.feeling} about it.`)
 }
+
+const searchTerm = "Apathetic"
+
+for (const note of notes) {
+    if (note.feeling === searchTerm) {
+        console.log (`When you were studying ${note.subject}, you were feeling ${note.feeling}`)
+    }
+}
+
+
+const latestNote = {
+    subject: "More Javascript",
+    date: "11/17/21",
+    feeling: "Weird",
+    timeSpent: 2, 
+}
+
+const createNote = (noteObject) => {
+    //I need to ge the length of the array to determine the index
+    const lastIndex = notes.length - 1
+    //once I have the index, I can determine what the last object is and what it's id is
+    const mostRecentId = notes[lastIndex].id
+    console.log(mostRecentId)
+    //once i have the latest note's id, I can increase it by 1 and then push the note into the arrary
+    noteObject.id = mostRecentId + 1
+    notes.push(noteObject)
+} 
+
+createNote(latestNote)
+
+for (const note of notes) {
+    console.log(note.id)
+}
